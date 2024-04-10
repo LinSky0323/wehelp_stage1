7c490d755d0c7831bede26890d11ba679d4900ce
LinSky0323 <a3822584@gmail.com>
2024-04-10 12:57:24 +0800

    照pylnt修改code

--- week2/main.py
+++ week2/main.py
@@ -1,11 +1,31 @@
+"""week2作業"""
     #第一題
 
-def find_and_print(messages, current_station):
+def find_and_print(messages1, current_station):
+    """找出離輸入的站點最近的人"""
     #列出捷運線
-    mrt=["Songshan","Nanjing Sanmin","Taipei Arena","Nanjing Fuxing","Songjiang Nanjin","Zhongshan","Beimen","Ximen","Xiaonanmen","Chiang Kai-Shek Memorial Hall","Guting","Taipower Building","Gongguan","Wanlong","Jingmei","Dapinglin","Qizhang","Xindian City Hall","Xindian"]
+    mrt=["Songshan",
+         "Nanjing Sanmin",
+         "Taipei Arena",
+         "Nanjing Fuxing",
+         "Songjiang Nanjin",
+         "Zhongshan",
+         "Beimen",
+         "Ximen",
+         "Xiaonanmen",
+         "Chiang Kai-Shek Memorial Hall",
+         "Guting",
+         "Taipower Building",
+         "Gongguan",
+         "Wanlong",
+         "Jingmei",
+         "Dapinglin",
+         "Qizhang",
+         "Xindian City Hall",
+         "Xindian"]
     #列出所有人在哪個站(用數字表示)
     local={}
-    for key,value in messages.items():
+    for key,value in messages1.items():
         if value.find("Xiaobitan")!=-1:
             local[key]=999
         for j in mrt:
@@ -24,7 +44,6 @@ def find_and_print(messages, current_station):
             ans=key
             dis=dist
     print(ans)
-        
 # your code here
 messages={
 "Leslie":"I'm at home near Xiaobitan station.",
@@ -46,13 +65,14 @@ find_and_print(messages, "Xindian City Hall") # print Vivian
 
 #可訂人 已訂時間
 AlreadyBook={}
-def book(consultants, hour, duration, criteria):
-    for i in consultants:
+def book(consultants1, hour, duration, criteria):
+    """找出可預約的人"""
+    for i in consultants1:
         if  i["name"] not in AlreadyBook:
             AlreadyBook[i["name"]]=[]
     #排出需求
     require=[]
-    for i in consultants:
+    for i in consultants1:
         require.append(i[criteria])
     #挑人
     while True:
@@ -66,22 +86,20 @@ def book(consultants, hour, duration, criteria):
         #看人該時段有沒有預約
         check=True
         for i in range(duration):
-            if hour+i in AlreadyBook[consultants[th]["name"]]:
+            if hour+i in AlreadyBook[consultants1[th]["name"]]:
                 check=False
                 break
         #沒預約 把時間塞進AlreadyBook 印出可預約人
-        if check==True:
+        if check:
             for i in range(duration):
-                AlreadyBook[consultants[th]["name"]].append(hour+i)
-            print(consultants[th]["name"])
+                AlreadyBook[consultants1[th]["name"]].append(hour+i)
+            print(consultants1[th]["name"])
             break
        # 全滿了
         if max(require)==0 or min(require)==float("inf"):
             print("No Service")
             break
 
-    
-    
 # your code here
 consultants=[
 {"name":"John", "rate":4.5, "price":1000},
@@ -98,6 +116,7 @@ book(consultants, 14, 3, "price") # John
 
     #第三題
 def func(*data):
+    """找出middle最少的人名"""
     #把大家的middle排出來
     middle=[]
     for i in data:
@@ -113,24 +132,23 @@ def func(*data):
         else:
             count[i]=1
     #存最小數
-    minV=float("inf")
+    min_v=float("inf")
     #最小有沒有重複
     repeat=False
     #存最小的key
-    minK=""
+    min_k=""
     for key,value in count.items():
-        if value<minV:
-            minV=value
-            minK=key
+        if value<min_v:
+            min_v=value
+            min_k=key
             repeat=False
-        elif value==minV:
+        elif value==min_v:
             repeat=True
     if repeat:
         print("沒有")
     else:
-        print(data[middle.index(minK)])
+        print(data[middle.index(min_k)])
 
-        
 # your code here
 func("彭大牆", "陳王明雅", "吳明") # print 彭大牆
 func("郭靜雅", "王立強", "郭林靜宜", "郭立恆", "林花花") # print 林花花
@@ -139,6 +157,7 @@ func("郭宣雅", "夏曼藍波安", "郭宣恆") # print 夏曼藍波安
 
     #第四題
 def get_number(index):
+    """找出數字"""
     print(index*4-index//3*5)
 # your code here
 get_number(1) # print 4
@@ -149,15 +168,16 @@ get_number(30) # print 70
 
     #第五題
 def find(spaces, stat, n):
+    """將人塞進有載客且最不浪費空間的車廂"""
     #列出能坐的車廂
-    canDrive=[]
-    for i in range(len(stat)):
+    can_drive=[]
+    for i in enumerate(stat):
         if stat[i]==1:
-            canDrive.append(i)
+            can_drive.append(i)
     #看哪節車廂最適合
     empty=float("inf")
     ans=-1
-    for i in canDrive:
+    for i in can_drive:
         if spaces[i]-n>=0 and spaces[i]-n<empty:
             empty=spaces[i]-n
             ans=i
