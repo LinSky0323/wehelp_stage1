9752a35ae0e620a7784aeb96e0de0f8741b25088
LinSky0323 <a3822584@gmail.com>
2024-04-16 00:02:27 +0800

    2 OK

--- week3/main.py
+++ week3/main.py
@@ -1,9 +1,10 @@
 """W3作業"""
-#第一題
+# #第一題
 
-#引入request、json
+# #引入request、json
 from urllib import request
 import json
+import bs4
 URL1="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
 URL2="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-2"
 #建立兩個連結的request
@@ -31,3 +32,48 @@ for i in range(data1["data"]["count"]):
 with open ("./mrt.csv","w",encoding="utf-8") as file1:
     for key,value in j_list.items():
         file1.write(key+value+"\n")
+
+# 第二題
+with open("./article.csv","w",encoding="utf-8") as file2:
+    def getData(URL3):
+    #建立request物件，附加request header屬性
+        req=request.Request(URL3,headers={
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+            "cookie":"over18=1"})
+        with request.urlopen(req) as response3:
+            data3=response3.read().decode("utf-8")
+        #喝湯
+        root=bs4.BeautifulSoup(data3,"html.parser")
+        #找到class=title的div 裡面的string是要找的標題
+        titles=root.find_all("div",class_="title")
+        #在頁面裡點進每個標題
+        for i in titles:
+            if i.a is not None:
+                file2.write(i.a.string+",")
+                #對點進的內容網頁在建立一個Request
+                reqContent=request.Request("https://www.ptt.cc"+i.a["href"],headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+                "cookie":"over18=1"})
+                with request.urlopen(reqContent) as response:
+                    data=response.read().decode("utf-8")
+                root2=bs4.BeautifulSoup(data,"html.parser")
+                #算like的數量
+                like=len(root2.find_all("span",string="推 "))
+                #算dislike的數量
+                dislike=len(root2.find_all("span",string="噓 "))
+                file2.write(str(like)+",")
+                file2.write(str(dislike)+",")
+                #找到內文為時間的span
+                times=root2.find("span",string="時間")
+                if times is None:
+                    file2.write(" "+"\n")
+                else:
+                    #用函數找到下一個span(就是顯示時間的)
+                    file2.write(times.find_next_sibling().string+"\n")
+        change_page=root.find("a",string="‹ 上頁")
+        return change_page["href"]
+    URL3="https://www.ptt.cc/bbs/Lottery/index.html"
+    count=0
+    #跑三頁
+    while count<3 :
+        URL3="https://www.ptt.cc"+getData(URL3)
+        count+=1
